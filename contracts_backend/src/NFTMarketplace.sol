a10a616b788511b0b1db634f413e5fb95717e854
valecha-a <145052407+valecha-a@users.noreply.github.com>
2024-08-15 15:53:43 -0400

    Fixed dependencies and installed packages

--- contracts_backend/src/NFTMarketplace.sol
+++ contracts_backend/src/NFTMarketplace.sol
@@ -132,7 +132,7 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "my-nft-marketplace/contracts_backend/lib/openzeppelin-contracts/contracts/utils/Counters.sol";
+import "my-nft-marketplace/contracts_backend/artifacts/contracts/@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
